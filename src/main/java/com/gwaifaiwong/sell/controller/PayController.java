b4bb530c6ba0da168dd632d52266114ac9ee6df9
gwaifaiwong <wang8guihui@gmail.com>
2020-07-26 22:35:19 +0800

    8-6 微信异步通知

--- src/main/java/com/gwaifaiwong/sell/controller/PayController.java
+++ src/main/java/com/gwaifaiwong/sell/controller/PayController.java
@@ -8,9 +8,7 @@ import com.gwaifaiwong.sell.service.PayService;
 import com.lly835.bestpay.model.PayResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
@@ -32,9 +30,9 @@ public class PayController {
     private PayService payService;
 
     @GetMapping("/create")
-    public ModelAndView create(@RequestParam("orderId")String orderId,
-                               @RequestParam("returnUrl")String returnUrl,
-                               Map<String,Object> map){
+    public ModelAndView create(@RequestParam("orderId") String orderId,
+                               @RequestParam("returnUrl") String returnUrl,
+                               Map<String, Object> map) {
         //查询订单
         OrderDTO orderDTO = orderService.findOne(orderId);
         if (orderDTO == null) {
@@ -44,6 +42,15 @@ public class PayController {
         PayResponse payResponse = payService.create(orderDTO);
         map.put("payResponse", payResponse);
         map.put("returnUrl", returnUrl);
-        return  new ModelAndView("pay/create",map);
+        return new ModelAndView("pay/create", map);
+    }
+
+    @PostMapping("/notify")
+    public ModelAndView notify(@RequestBody String notifyData) {
+        payService.notify(notifyData);
+        //返回微信处理结果
+        return new ModelAndView("pay/success");
+
+
     }
 }
--- src/main/java/com/gwaifaiwong/sell/service/impl/PayServiceImpl.java
+++ src/main/java/com/gwaifaiwong/sell/service/impl/PayServiceImpl.java
@@ -1,8 +1,12 @@
 package com.gwaifaiwong.sell.service.impl;
 
 import com.gwaifaiwong.sell.dto.OrderDTO;
+import com.gwaifaiwong.sell.enums.ResultEnum;
+import com.gwaifaiwong.sell.exception.SellException;
+import com.gwaifaiwong.sell.service.OrderService;
 import com.gwaifaiwong.sell.service.PayService;
 import com.gwaifaiwong.sell.utils.JsonUtil;
+import com.gwaifaiwong.sell.utils.MathUtil;
 import com.lly835.bestpay.enums.BestPayTypeEnum;
 import com.lly835.bestpay.model.PayRequest;
 import com.lly835.bestpay.model.PayResponse;
@@ -26,6 +30,9 @@ public class PayServiceImpl implements PayService {
     @Autowired
     private BestPayServiceImpl bestPayService;
 
+    @Autowired
+    private OrderService orderService;
+
     @Override
     public PayResponse create(OrderDTO orderDTO) {
         PayRequest payRequest = new PayRequest();
@@ -37,7 +44,37 @@ public class PayServiceImpl implements PayService {
         log.info("【微信支付】 request={}", JsonUtil.toJson(payRequest));
 
         PayResponse payResponse = bestPayService.pay(payRequest);
-        log.info("【微信支付】 response={}",JsonUtil.toJson(payResponse));
-        return  payResponse;
+        log.info("【微信支付】 response={}", JsonUtil.toJson(payResponse));
+        return payResponse;
+    }
+
+    @Override
+    public PayResponse notify(String notifyData) {
+        //1. 验证签名
+        //2. 支付的状态
+        //3. 支付金额
+        //4. 支付人（下单人==支付人）
+
+        PayResponse payResponse = bestPayService.asyncNotify(notifyData);
+        log.info("【微信支付】异步通知，payResponse={}", JsonUtil.toJson(payResponse));
+
+        //查询订单
+        OrderDTO orderDTO = orderService.findOne(payResponse.getOrderId());
+        //判断订单是否存在
+        if (orderDTO == null) {
+            log.error("【微信支付】异步通知，订单不存在，orderId={}", payResponse.getOrderId());
+            throw new SellException(ResultEnum.ORDER_NOT_EXIT);
+        }
+        //判断金额是否一致
+        if (!MathUtil.equals(payResponse.getOrderAmount(),orderDTO.getOrderAmount().doubleValue())) {
+            log.error("【微信支付】异步通知，订单金额不一致，orderId={},微信通知金额={}，系统金额={}",
+                    payResponse.getOrderId(),
+                    payResponse.getOrderAmount(),
+                    orderDTO.getOrderAmount());
+            throw new SellException(ResultEnum.WXPAY_NOTIFY_MONEY_VERIFY_ERROR);
+        }
+        //修改订单的支付状态
+        orderService.paid(orderDTO);
+        return payResponse;
     }
 }
