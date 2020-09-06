43db6ababba85cc2098f75349f8cd3823177b1c9
gwaifaiwong <wang8guihui@gmail.com>
2020-09-06 22:07:06 +0800

    12-7 登出成功

--- src/main/java/com/gwaifaiwong/sell/controller/SellerUserController.java
+++ src/main/java/com/gwaifaiwong/sell/controller/SellerUserController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.UUID;
@@ -63,7 +65,21 @@ public class SellerUserController {
     }
 
     @GetMapping("/logout")
-    public void logout() {
+    public ModelAndView logout(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Map<String, Object> map) {
+        //1. 从cookie里查询
+        Cookie cookie = CookieUtil.get(request, CookieConstant.TOKEN);
+        if (cookie != null) {
+            //2. 清楚redis
+            redisTemplate.opsForValue().getOperations().delete(String.format(RedisConstant.TOKEN_PREFIX,cookie.getValue()));
+            //3. 清楚cookie
+            CookieUtil.set(response,CookieConstant.TOKEN,null,0);
+        }
+
+        map.put("msg", ResultEnum.LOGOUT.getMsg());
+        map.put("url","/sell/seller/order/list");
+        return new ModelAndView("common/success", map);
 
     }
 }
--- src/main/java/com/gwaifaiwong/sell/enums/ResultEnum.java
+++ src/main/java/com/gwaifaiwong/sell/enums/ResultEnum.java
@@ -43,6 +43,8 @@ public enum ResultEnum {
     PRODUCT_STATUS_ERROR(24,"商品状态不正确"),
 
     LOGIN_FAIL(25,"登录失败，登录信息不正确"),
+
+    LOGOUT(26,"登出成功"),
     ;
 
     private Integer code;
